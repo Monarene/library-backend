7683d34fbe7091a604f15666c3c37fa819b87a82
Michael Mekuleyi <mekuleyimichael@gmail.com>
2020-07-18 08:46:10 +0100

    added the select one route

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const bookRouter = express.Router();
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectID } = require("mongodb");
 const debug = require("debug")("app:bookRoutes");
 
 function router(nav) {
-  const books = [
+  /* const books = [
     {
       title: "War and Peace",
       genre: "Historical Fiction",
@@ -29,7 +29,7 @@ function router(nav) {
       author: "Lev Niko Tolstoy",
       read: false,
     },
-  ];
+  ]; */
 
   bookRouter.route("/").get((req, res) => {
     const url = "mongodb://localhost:27017";
@@ -57,18 +57,31 @@ function router(nav) {
     })();
   });
 
-  bookRouter.route("/single").get((req, res) => {
-    debug("");
-    res.send("Hello Single books");
-  });
-
   bookRouter.route("/:id").get((req, res) => {
     const { id } = req.params;
-    res.render("bookView", {
-      nav,
-      title: "Library",
-      book: books[id],
-    });
+    const url = "mongodb://localhost:27017";
+    const dbname = "libraryApp";
+    (async function mongo() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+        debug("Connected correctly to server");
+
+        const db = client.db(dbname);
+        const col = await db.collection("books");
+        const book = await col.findOne({ _id: new ObjectID(id) });
+        debug(book);
+        res.render("bookView", {
+          nav,
+          title: "Library",
+          book,
+        });
+      } catch (err) {
+        debug(err.stack);
+      }
+
+      client.close();
+    })();
   });
 
   return bookRouter;
